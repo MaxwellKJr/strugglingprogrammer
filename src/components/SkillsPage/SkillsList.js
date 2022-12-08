d912793148034683e4f873b48779131bdcf121f8
Maxwell Kapezi, Jr <maxwellkjr@gmail.com>
2022-12-09 00:09:22 +0200

    Added 'What I Use' section to the skills page

--- src/components/SkillsPage/SkillsList.js
+++ src/components/SkillsPage/SkillsList.js
@@ -3,21 +3,23 @@ import './SkillsList.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faCss3,
+	faFirefoxBrowser,
 	faGitAlt,
 	faHtml5,
 	faJava,
 	faJs,
 	faLaravel,
+	faLinux,
 	faPhp,
 	faPython,
 	faReact,
 	faWordpress,
 } from '@fortawesome/free-brands-svg-icons';
-import { faDatabase, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faDatabase, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 const skillsList = [
 	{
-		name: 'Javascript',
+		name: 'JavaScript',
 		icon: faJs,
 	},
 	{
@@ -54,17 +56,70 @@ const skillsList = [
 	},
 ];
 
+const uses = [
+	{
+		type: 'Operating System',
+		name: 'EndeavourOS',
+		description: 'An Arch Based Linux Distribution',
+		whyUse:
+			'Since 2019, I have fallen in love with the Linux ecosystem and emphasis of FREE and OPEN SOURCE SOFTWARE (FOSS).',
+		icon: faLinux,
+	},
+
+	{
+		type: 'IDE',
+		name: 'WebStorm',
+		description: 'The SMARTEST JavaScript IDE from our beloved JetBrains.',
+		whyUse:
+			'After using VS Code for a long time, I settled for this because it basically gives me superpowers.',
+		icon: faCode,
+	},
+
+	{
+		type: 'Internet Browser',
+		name: 'Firefox',
+		description: 'The BEST browser out there.',
+		whyUse:
+			'It is free and open source, secure, and BLAZINGLY faster than any chromium based browser out there.',
+		icon: faFirefoxBrowser,
+	},
+];
+
 const SkillsList = () => {
 	return (
-		<div>
-			<p>Languages/Technologies I am comfortable with:</p>
-			<ul className='skills-grid'>
-				{skillsList.map(skill => (
-					<li className='card-dark'>
-						{skill.name} <FontAwesomeIcon icon={skill.icon} size='lg' />
-					</li>
-				))}
-			</ul>
+		<div className='split'>
+			<div className='skills'>
+				<p>Languages/Technologies:</p>
+				<ul className='skills-grid'>
+					{skillsList.map(skill => (
+						<li className='card-dark'>
+							{skill.name} <FontAwesomeIcon icon={skill.icon} size='lg' />
+						</li>
+					))}
+				</ul>
+			</div>
+
+			<div>
+				<h1>What I Use:</h1>
+				<ul className='uses-grid'>
+					{uses.map(use => (
+						<li className='use-card'>
+							<li className='use-type'>
+								<h5 className='type'>{use.type}: </h5>
+								<h3>
+									{use.name} <FontAwesomeIcon icon={use.icon} />
+								</h3>
+							</li>
+							<ul className='use-description'>
+								<li>{use.description}</li>
+								<li>
+									<h5>Why I Use This:</h5> {use.whyUse}
+								</li>
+							</ul>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
