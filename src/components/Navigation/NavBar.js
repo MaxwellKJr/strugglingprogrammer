dbcdd471f1cdd2bfeea1b933f1940744489db8ed
maxwellkjr <maxwellkjr@gmail.com>
2023-04-13 21:54:23 +0200

    NavBar logo fix

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -6,32 +6,32 @@ import SocialLinks from '../SocialLinks/SocialLinks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavBar = () => {
-	const links = pageLinks;
-	const date = new Date();
+  const links = pageLinks;
+  const date = new Date();
 
-	return (
-		<nav id='main-nav'>
-			<div className='container'>
-				<Link to='/' className='logo'>
-					MJ <br /> Codes
-				</Link>
-				<ul className='nav-links'>
-					{links.map(link => (
-						<li key={link.name} className='nav-link'>
-							<Link to={link.route} activeClassName='active' title={link.title}>
-								<FontAwesomeIcon icon={link.icon} className='route-icon' />
-								<span className='route-text'>{link.name}</span>
-							</Link>
-						</li>
-					))}
-				</ul>
-				<SocialLinks />
-				<div id='header-footer'>
-					<small>{date.getFullYear()} &copy; MJ Codes</small>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav id='main-nav'>
+      <div className='container'>
+        <Link to='/' className='logo'>
+          <span className='logo-text'>MJ <br /> Codes</span>
+        </Link>
+        <ul className='nav-links'>
+          {links.map(link => (
+            <li key={link.name} className='nav-link'>
+              <Link to={link.route} activeClassName='active' title={link.title}>
+                <FontAwesomeIcon icon={link.icon} className='route-icon' />
+                <span className='route-text'>{link.name}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <SocialLinks />
+        <div id='header-footer'>
+          <small>{date.getFullYear()} &copy; MJ Codes</small>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
--- src/components/SkillsPage/SkillsList.js
+++ src/components/SkillsPage/SkillsList.js
@@ -5,48 +5,48 @@ import toolsList from './Tools';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const SkillsList = () => {
-	const skills = skillsList;
-	const uses = toolsList;
+  const skills = skillsList;
+  const uses = toolsList;
 
-	return (
-		<div className='split'>
-			<div className='skills'>
-				<p>Languages/Technologies:</p>
-				<ul className='skills-grid'>
-					{skills.map(skill => (
-						<li key={skill.name} className='card-dark'>
-							{skill.name} <FontAwesomeIcon icon={skill.icon} size='lg' />
-						</li>
-					))}
-				</ul>
-			</div>
+  return (
+    <div className='split'>
+      <div className='skills'>
+        <p>Languages/Technologies:</p>
+        <ul className='skills-grid'>
+          {skills.map(skill => (
+            <li key={skill.name} className='card-dark'>
+              {skill.name} <FontAwesomeIcon icon={skill.icon} size='lg' />
+            </li>
+          ))}
+        </ul>
+      </div>
 
-			{/* Skills */}
-			<div>
-				<h1>What I Use:</h1>
-				<ul className='uses-grid'>
-					{uses.map(use => (
-						<li key={use.name} className='use-card'>
-							<li className='use-type'>
-								<h5 className='type'>{use.type}: </h5>
-								<h3>
-									{use.name} <FontAwesomeIcon icon={use.icon} />
-								</h3>
-							</li>
-							<ul className='use-description'>
-								<li>
-									<em>{use.description}</em>
-								</li>
-								<li>
-									<h5>Why I Use This:</h5> {use.whyUse}
-								</li>
-							</ul>
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	);
+      {/* Skills */}
+      <div>
+        <h1>What I Use:</h1>
+        <ul className='uses-grid'>
+          {uses.map(use => (
+            <li key={use.name} className='use-card'>
+              <ul className='use-type'>
+                <li><h5 className='type'>{use.type}: </h5></li>
+                <li><h3>
+                  {use.name} <FontAwesomeIcon icon={use.icon} />
+                </h3></li>
+              </ul>
+              <ul className='use-description'>
+                <li>
+                  <em>{use.description}</em>
+                </li>
+                <li>
+                  <h5>Why I Use This:</h5> {use.whyUse}
+                </li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SkillsList;
