9d341434549f6c1b0f766ea1cc3c2b1a2c31fc58
maxwellkjr <maxwellkjr@gmail.com>
2023-04-13 19:51:26 +0200

    Added animations and page transitions with framer-motion

--- src/components/HomePage/Showcase.js
+++ src/components/HomePage/Showcase.js
@@ -1,54 +1,109 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import './Showcase.css';
+import { motion } from 'framer-motion'
 
 const occupations = [
-	{
-		name: 'Software Developer💻',
-		url: 'https://maxwellkapezi-resume.netlify.app',
-	},
-	{
-		name: 'Writer📝',
-		url: 'https://malawianwriter.vercel.app',
-	},
-	{
-		name: 'Graphic Designer🖌️',
-		url: 'https://instagram.com/_maxwellkjr',
-	},
+  {
+    name: 'Software Developer💻',
+    url: 'https://maxwellkapezi-resume.netlify.app',
+  },
+  {
+    name: 'Writer📝',
+    url: 'https://malawianwriter.vercel.app',
+  },
+  {
+    name: 'Graphic Designer🖌️',
+    url: 'https://instagram.com/_maxwellkjr',
+  },
 ];
 
 const Showcase = () => (
-	<section id='showcase'>
-		<div className='container'>
-			<h1 id='slogan' style={{ textAlign: 'start' }}>
-				The Struggling Programmer
-			</h1>
-			<ul className='occupations'>
-				{occupations.map(occupation => (
-					<li key={occupation.name}>
-						<a
-							href={occupation.url}
-							target='_blank'
-							rel='noopener noreferrer'
-							className='occupation'
-						>
-							{occupation.name}
-						</a>
-					</li>
-				))}
-			</ul>
-			<p>A platform to showcase my software development journey and share the few things I know.</p>
+  <section id='showcase'>
+    <div className='container'>
+      <h1 id='slogan' style={{ textAlign: 'start' }}>
+        The Struggling Programmer
+      </h1>
+      <motion.ul className='occupations'
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            x: -100,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            x: 0,
+            opacity: 1,
+            transition: {
+              delay: 0.5,
+            },
+          },
+        }}
+      >
+        {occupations.map(occupation => (
+          <li key={occupation.name}>
+            <a
+              href={occupation.url}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='occupation'
+            >
+              {occupation.name}
+            </a>
+          </li>
+        ))}
+      </motion.ul>
+      <motion.p
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            x: -100,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            x: 0,
+            opacity: 1,
+            transition: {
+              delay: 0.8,
+            },
+          },
+        }}
+      >A platform to showcase my software development journey and share the few things I know.</motion.p>
 
-			<div className='ctas'>
-				<Link to='/projects' className='btn btn-primary'>
-					View Projects
-				</Link>
-				<Link to='/about' className='btn btn-secondary'>
-					About Me
-				</Link>
-			</div>
-		</div>
-	</section>
+      <motion.div className='ctas'
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            x: -100,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            x: 0,
+            opacity: 1,
+            transition: {
+              delay: 1.2,
+            },
+          },
+        }}
+      >
+        <Link to='/projects' className='btn btn-primary'>
+          View Projects
+        </Link>
+        <Link to='/about' className='btn btn-secondary'>
+          About Me
+        </Link>
+      </motion.div>
+    </div>
+  </section>
 );
 
 export default Showcase;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import NavBar from '../components/Navigation/NavBar';
 import './Layout.css';
+import { motion } from 'framer-motion'
 
 export default function Layout({ children }) {
   return (
     <>
       <NavBar />
       <main>
-        <div className='container'>{children}</div>
+        <motion.div
+          className='container'
+          initial='hidden'
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 1,
+              y: 50,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              y: 0,
+              opacity: 1,
+            },
+          }}
+        >
+          {children}
+        </motion.div>
       </main>
     </>
   );
