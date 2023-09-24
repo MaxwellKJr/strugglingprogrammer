e75617768b15ae9449de938bffc89dd4afe2f6b6
MaxwellKJr <maxwellkjr@gmail.com>
2023-09-24 14:32:21 +0200

    Added glitch on hover effect to the #slogan

--- src/components/HomePage/Showcase.tsx
+++ src/components/HomePage/Showcase.tsx
@@ -22,116 +22,125 @@ const occupations = [
 const occupationsList = OccupationsList;
 
 const Showcase = () => {
-const [randomWord, setRandomWord] = useState("");
+  const [randomWord, setRandomWord] = useState("");
 
-useEffect(() => {
-  const randomIndex = Math.floor(Math.random() * occupationsList.length)
-  const selectedWord = occupationsList[randomIndex];
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * occupationsList.length)
+    const selectedWord = occupationsList[randomIndex];
 
-  setRandomWord(selectedWord);
-}, []);
-return(
-  <section id='showcase'>
-    <span className="logo">{`MJ Codes_`}</span>
-    <div className='container'>
-      <motion.h1 id='slogan' style={{ textAlign: 'start' }}
-        initial='hidden'
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 1,
-            y: 50,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            y: 0,
-            opacity: 1,
-          },
-        }}
-      >
-        The {randomWord} Programmer
-      </motion.h1>
-      <motion.ul className='occupations'
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            x: -100,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            x: 0,
-            opacity: 1,
-            transition: {
-              delay: 0.5,
+    setRandomWord(selectedWord);
+  }, []);
+  return (
+    <section id='showcase'>
+      <span className="logo">{`MJ Codes_`}</span>
+      <div className='container'>
+        <motion.h1 id='slogan' data-text={`The ${randomWord} Developer`} style={{ textAlign: 'start' }}
+          initial='hidden'
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 1,
+              x: -200,
+              opacity: 0,
             },
-          },
-        }}
-      >
-        {occupations.map(occupation => (
-          <li key={occupation.name}>
-            <a
-              href={occupation.url}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='occupation'
-            >
-              {occupation.name}
-            </a>
-          </li>
-        ))}
-      </motion.ul>
-      <motion.p
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            x: -100,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            x: 0,
-            opacity: 1,
-            transition: {
-              delay: 0.8,
+            visible: {
+              scale: 1,
+              x: 0,
+              opacity: 1,
+              transition: {
+                delay: 0,
+              }
             },
-          },
-        }}
-      >A platform to showcase my software development journey and share the few things I know.</motion.p>
+          }}
+        >
+          The <span className='randomOccupation'>{randomWord}</span> Developer
+        </motion.h1>
+        <motion.hr
+          initial={{ width: '0%' }} // Initial width set to 0%
+          animate={{ width: '50%' }} // Animate to full width
+          transition={{ duration: 1.5, delay: 0.6 }} // Animation duration and delay
+          className='progressBar'
+        />
+        <motion.ul className='occupations'
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              x: -100,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              x: 0,
+              opacity: 1,
+              transition: {
+                delay: 0.5,
+              },
+            },
+          }}
+        >
+          {occupations.map(occupation => (
+            <li key={occupation.name}>
+              <a
+                href={occupation.url}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='occupation'
+              >
+                {occupation.name}
+              </a>
+            </li>
+          ))}
+        </motion.ul>
+        <motion.p
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              x: -100,
+              opacity: 0,
+            },
+            visible: {
+              scale: 1,
+              x: 0,
+              opacity: 1,
+              transition: {
+                delay: 0.8,
+              },
+            },
+          }}
+        >A platform to showcase my software development journey and share the few things I know.</motion.p>
 
-      <motion.div className='ctas'
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            x: -100,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            x: 0,
-            opacity: 1,
-            transition: {
-              delay: 1.2,
+        <motion.div className='ctas'
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              // scale: 0.8,
+              y: 100,
+              opacity: 0,
             },
-          },
-        }}
-      >
-        <Link to='/projects' className='btn btn-primary'>
-          View Projects
-        </Link>
-        <Link to='/about' className='btn btn-secondary'>
-          About Me
-        </Link>
-      </motion.div>
-    </div>
-  </section>
-);
-      }
+            visible: {
+              // scale: 1,
+              y: 0,
+              opacity: 1,
+              transition: {
+                delay: 1.2,
+              },
+            },
+          }}
+        >
+          <Link to='/projects' className='btn btn-primary'>
+            View Projects
+          </Link>
+          <Link to='/about' className='btn btn-secondary'>
+            About Me
+          </Link>
+        </motion.div>
+      </div>
+    </section >
+  );
+}
 export default Showcase;
