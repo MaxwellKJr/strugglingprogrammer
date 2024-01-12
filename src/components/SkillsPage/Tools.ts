f1c2bfa68d9e8f82a31409525ec747494b376ab9
MaxwellKJr <maxwellkjr@gmail.com>
2024-01-12 03:17:01 +0200

    Updated tools page. Removed LunarVim and added LazyVim as my code editor of choice

--- src/components/SkillsPage/Tools.ts
+++ src/components/SkillsPage/Tools.ts
@@ -1,58 +1,52 @@
-import { faAndroid, faFirefoxBrowser, faLinux } from '@fortawesome/free-brands-svg-icons';
-import { faCode, faGlobe, faTerminal } from '@fortawesome/free-solid-svg-icons';
+import { faAndroid, faLinux } from "@fortawesome/free-brands-svg-icons";
+import { faCode, faGlobe, faTerminal } from "@fortawesome/free-solid-svg-icons";
 
 const toolsList = () => [
-  // {
-  //   type: "Laptop",
-  //   name: "HP 250 G6 Notebook PC",
-  //   link: "https://www.amazon.com/hp-250-g6/s?k=hp+250+g6",
-  //   description: "A basic i3 6th-Gen laptop with 8GB DDR4 RAM and a 256GB SSD, nothing fancy here",
-  //   whyUse:
-  //     "It was what I could afford then (around 2019/2020) but it still gets the job done. No excuses.",
-  //   icon: faLaptop,
-  // },
   {
-    type: 'Operating System',
-    name: 'Nobara OS',
-    link: 'https://nobaraproject.org/',
-    description: `Gamer Centred OS based on Fedora Workstation`,
+    type: "Operating System",
+    name: "Nobara OS ",
+    link: "https://nobaraproject.org/",
+    description: "Gamer Centred OS based on Fedora Workstation",
     whyUse:
-      'Since 2019, I have fallen in love with the Linux ecosystem and its emphasis on FREE and OPEN SOURCE SOFTWARE (FOSS).',
+      "Since 2019, I have fallen in love with the Linux ecosystem and its emphasis on FREE and OPEN SOURCE SOFTWARE (FOSS).",
     icon: faLinux,
   },
   {
-    type: 'Terminal Emulator',
-    name: 'Alacritty',
-    link: 'https://github.com/alacritty/alacritty',
-    description: 'A fast, cross-platform, OpenGL terminal emulator',
+    type: "Terminal Emulator",
+    name: "Alacritty",
+    link: "https://github.com/alacritty/alacritty",
+    description: "A fast, cross-platform, OpenGL terminal emulator",
     whyUse:
       "It's light-weight and blazingly fast, offers great customization options. Only drawback is that it does not have support for tabs but Tmux covers that.",
     icon: faTerminal,
   },
   {
-    type: 'Code Editor',
-    name: 'LunarVim',
-    link: 'https://www.lunarvim.org/',
-    description: 'A neovim distribution with an IDE layer and sane defaults.',
+    type: "Code Editor",
+    name: "LazyVim ",
+    link: "https://www.lazyvim.org/",
+    description:
+      "LazyVim is a Neovim setup powered by 💤 'lazy.nvim' that makes it easy to customize and extend my config.",
     whyUse:
-      'It does everything that I would require from VS Code but is lighter and FASTER. Of course learning VIM/Neovim takes time.',
+      "It does everything that I would require from VS Code but is lighter and FASTER. Of course learning VIM/Neovim takes time.",
     icon: faCode,
   },
   {
-    type: 'IDE',
-    name: 'Android Studio',
-    link: 'https://developer.android.com/studio',
-    description: 'The official Integrated Development Environment for Android app development',
-    whyUse: 'I think this self explanatory. However, for months now, Android Studio has just been something I use because it provides the necessary tools (SDKs and the like) to develop android apps, I do most of my Flutter and RN development in LunarVim rather than Android Studio.',
+    type: "IDE",
+    name: "Android Studio ",
+    link: "https://developer.android.com/studio",
+    description:
+      "The official Integrated Development Environment for Android app development",
+    whyUse:
+      "I think this self explanatory. However, for months now, Android Studio has just been something I use because it provides the necessary tools (SDKs and the like) to develop android apps, I do most of my Flutter and RN development in LunarVim rather than Android Studio.",
     icon: faAndroid,
   },
   {
     type: "Internet Browser",
-    name: "Vivaldi",
+    name: "Vivaldi ",
     link: "https://vivaldi.com/",
     description: "Powerful. Personal. Private.",
     whyUse:
-      "Vivaldi Browser is chromium based and is for power users who spend most of their time on the web/browser. As someone who always has about 20+ tabs open, Vivaldi great tab management tools that I could not find anywhere else. Sorry, Firefox.",
+      "Vivaldi Browser is chromium based and is for power users who spend most of their time on the web/browser. As someone who always has about 20+ tabs open, I fell in love with Vivaldi's great tab management tools that I could not find in other browsers. Sorry, Firefox.",
     icon: faGlobe,
   },
 ];
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,47 +1,47 @@
-import * as React from 'react';
-import { useStaticQuery, graphql, PageProps } from 'gatsby';
-import Blogs from '../components/BlogPage/Blogs';
-import Layout from '../layouts/index';
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Blogs from "../components/BlogPage/Blogs";
+import Layout from "../layouts/index";
 
 const Blog = () => {
   const data = useStaticQuery(graphql`
-		query {
-			allMarkdownRemark(
-				filter: { fileAbsolutePath: { regex: "/blog/" } }
-				sort: { frontmatter: { date: DESC } }
-			) {
-				edges {
-					node {
-						fields {
-							slug
-						}
-						frontmatter {
-							title
-							description
-							date(formatString: "dddd, D MMMM YYYY")
-							featuredImage {
-								childImageSharp {
-									gatsbyImageData(
-										aspectRatio: 1.5
-										placeholder: BLURRED
-										formats: [AUTO, WEBP, AVIF]
-									)
-								}
-							}
-							tags
-						}
-						timeToRead
-					}
-				}
-			}
-		}
+    query {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/blog/" } }
+        sort: { frontmatter: { date: DESC } }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              description
+              date(formatString: "dddd, D MMMM YYYY")
+              featuredImage {
+                childImageSharp {
+                  gatsbyImageData(
+                    aspectRatio: 1.5
+                    placeholder: BLURRED
+                    formats: [AUTO, WEBP, AVIF]
+                  )
+                }
+              }
+              tags
+            }
+            timeToRead
+          }
+        }
+      }
+    }
   `);
   const posts = data.allMarkdownRemark.edges;
 
   return (
     <Layout>
-      <section id='blog'>
-        <div className='container'>
+      <section id="blog">
+        <div className="container">
           <Blogs posts={posts} />
         </div>
       </section>
@@ -56,15 +56,29 @@ export const Head = () => {
     <>
       <title>{"MJ Codes | Blog"}</title>
       <meta name="title" content={"MJ Codes | Blog"} />
-      <meta name="description" content={"Maxwell Kapezi Jr's official website to showcase his software development journey and share the few things he knows."} />
+      <meta
+        name="description"
+        content={
+          "Maxwell Kapezi Jr's official website to showcase his software development journey and share the few things he knows."
+        }
+      />
       <meta name="og:title" content={"MJ Codes | Blog"} />
-      <meta name="og:description" content={"Maxwell Kapezi Jr's official website to showcase his software development journey and share the few things he knows."} />
+      <meta
+        name="og:description"
+        content={
+          "Maxwell Kapezi Jr's official website to showcase his software development journey and share the few things he knows."
+        }
+      />
       <meta name="og:url" content={"https://bymjcodes.com/blog/"} />
       <meta name="og:type" content="website" />
       <meta name="twitter:title" content={"MJ Codes | Blog"} />
-      <meta name="twitter:description" content={"Maxwell Kapezi Jr's official website to showcase his software development journey and share the few things he knows."} />
+      <meta
+        name="twitter:description"
+        content={
+          "Maxwell Kapezi Jr's official website to showcase his software development journey and share the few things he knows."
+        }
+      />
       <meta name="twitter:card" content="summary" />
     </>
   );
 };
-
