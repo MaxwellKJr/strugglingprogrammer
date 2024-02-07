8963675aa0b8b8799adcab6c097552869309805d
MaxwellKJr <maxwellkjr@gmail.com>
2024-02-07 10:54:22 +0200

    UI REFACTOR - Changed the blog-tags

--- src/components/BlogPage/BlogCard.tsx
+++ src/components/BlogPage/BlogCard.tsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
-import './BlogCard.css';
+import React from "react";
+import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import "./BlogCard.css";
 
 const BlogCard = ({ post }: any) => {
-  const { title, date, featuredImage, description, tags } = post.node.frontmatter;
+  const { title, date, featuredImage, description, tags } =
+    post.node.frontmatter;
   const { slug } = post.node.fields;
 
   return (
-    <div className='blog-card'>
-      <Link to={`${slug}`}>
-        <GatsbyImage image={getImage(featuredImage)} alt={title} title={title} />
-      </Link>
-      <div className='blog-details'>
-        <small className='timeToRead'>{`> ${post.node.timeToRead} minute read`}</small>
-        <h3>
-          <Link to={`${slug}`}>{title}</Link>
-        </h3>
-        <small className='date'>{date}</small>
-        <p>{description}</p>
-        <ul className='tags'>
-          {tags.map((tag: string) => (
-            <li key={tag} className='tag'>
-              <small>{tag}</small>
-            </li>
-          ))}
-        </ul>
+    <div className="blog-card">
+      <div>
+        <Link to={`${slug}`}>
+          <GatsbyImage
+            image={getImage(featuredImage)}
+            alt={title}
+            title={title}
+          />
+        </Link>
+        <div className="blog-details">
+          <small className="timeToRead">{`> ${post.node.timeToRead} minute read`}</small>
+          <h3>
+            <Link to={`${slug}`}>{title}</Link>
+          </h3>
+          <small className="date">{date}</small>
+          <p>{description}</p>
+        </div>
       </div>
+      <ul className="tags blog-tags">
+        {tags.map((tag: string) => (
+          <li key={tag} className="tag">
+            <small>{tag}</small>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
