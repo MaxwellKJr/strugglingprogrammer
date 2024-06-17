73253e0e8fe4ef49c2b4e24670939ac25c20a382
MaxwellKJr <maxwellkjr@gmail.com>
2024-06-17 10:20:58 +0200

    Scss value fix from "0" to 0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,54 +7,54 @@ import Showcase from "../components/HomePage/Showcase";
 import Layout from "../layouts/index";
 
 export const query = graphql`
-    query {
-        allSite {
-            nodes {
-                siteMetadata {
-                    siteUrl
-                    title
-                    description
-                }
-            }
+  query {
+    allSite {
+      nodes {
+        siteMetadata {
+          siteUrl
+          title
+          description
         }
+      }
     }
+  }
 `;
 
 const IndexPage = () => (
-	<Layout>
-		<Showcase/>
-	</Layout>
+  <Layout>
+    <Showcase />
+  </Layout>
 );
 
 export default IndexPage;
 
 export const Head = ({ data }: PageProps) => {
-	const seo = data.allSite.nodes[0];
+  const seo = data.allSite.nodes[0];
 
-	const { title, description, siteUrl } = seo.siteMetadata;
+  const { title, description, siteUrl } = seo.siteMetadata;
 
-	return (
-		<>
-			<title>{`${title} | Programming and Tech Related Content 🚀`}</title>
-			<meta
-				name="title"
-				content={`${title} | Programming and Tech Related Content 🚀`}
-			/>
-			<meta name="description" content={description}/>
-			<meta
-				property="og:title"
-				content={`${title} | Programming and Tech Related Content 🚀`}
-			/>
-			<meta property="og:description" content={description}/>
-			<meta
-				property="og:image"
-				content="https://i.ibb.co/QnrcfDX/Screenshot-from-2024-01-04-10-22-56.png"
-			/>
-			<meta property="og:url" content={siteUrl}/>
-			<meta property="og:type" content="website"/>
-			<meta name="twitter:title" content={title}/>
-			<meta name="twitter:description" content={description}/>
-			<meta name="twitter:card" content="summary"/>
-		</>
-	);
+  return (
+    <>
+      <title>{`${title} | Programming and Tech Related Content 🚀`}</title>
+      <meta
+        name="title"
+        content={`${title} | Programming and Tech Related Content 🚀`}
+      />
+      <meta name="description" content={description} />
+      <meta
+        property="og:title"
+        content={`${title} | Programming and Tech Related Content 🚀`}
+      />
+      <meta property="og:description" content={description} />
+      <meta
+        property="og:image"
+        content="https://i.ibb.co/QnrcfDX/Screenshot-from-2024-01-04-10-22-56.png"
+      />
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:card" content="summary" />
+    </>
+  );
 };
