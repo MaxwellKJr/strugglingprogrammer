f6142edef0b4fc3cd4cc6fede5c70296b838f0bd
MaxwellKJr <maxwellkjr@gmail.com>
2024-11-26 11:20:29 +0200

    Added toggle effects and transitions to FAQ cards

--- src/components/ServicesPage/FAQS.tsx
+++ src/components/ServicesPage/FAQS.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Services.css";
 
-const questionsAndAnswers = [
+interface Question {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+const questionsAndAnswers: Question[] = [
   {
     id: 1,
     question: "What are your hours of operation?",
@@ -15,14 +21,31 @@ const questionsAndAnswers = [
 ];
 
 export const FAQS = () => {
+  const [isOpen, setIsOpen] = useState<{ [key: number]: boolean }>({});
+
   return (
     <>
       <h2>Frequently Asked Questions</h2>
       <div className="questions-container">
         {questionsAndAnswers.map((question) => (
           <div className="question-card" key={question.id}>
-            <h4 className="question">{question.question}</h4>
-            <p className="answer">{question.answer}</p>
+            <h4
+              className="question"
+              onClick={() =>
+                setIsOpen((prevOpen) => ({
+                  ...prevOpen,
+                  [question.id]: !prevOpen[question.id],
+                }))
+              }
+            >
+              {question.question}{" "}
+              {isOpen[question.id] ? <span>-</span> : <span>+</span>}
+            </h4>
+            {isOpen[question.id] ? (
+              <p className="answer">{question.answer}</p>
+            ) : (
+              <p className="answer hidden">{question.answer}</p>
+            )}
           </div>
         ))}
       </div>
